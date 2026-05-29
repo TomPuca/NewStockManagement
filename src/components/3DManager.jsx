019b2c12a3153c11cd17148bc91585275f38a3f4
Tran Dinh Hung <trandinhhung@Trans-Mac-mini.local>
2026-05-29 08:40:29 +0700

    fix: switch to drive.google.com/thumbnail endpoint with sz=w1200 query parameter to fix image blurriness on 3x DPR displays

--- src/components/3DManager.jsx
+++ src/components/3DManager.jsx
@@ -27,8 +27,8 @@ const convertImageUrl = (url) => {
 
   if (driveFileMatch && driveFileMatch[1]) {
     const fileId = driveFileMatch[1];
-    // Use the lh3.googleusercontent.com CDN for reliable embedding, requesting high-resolution (1000px width)
-    return `https://lh3.googleusercontent.com/d/${fileId}=w1000`;
+    // Use the official/reliable thumbnail endpoint with standard query parameters to request high-res (1200px width)
+    return `https://drive.google.com/thumbnail?id=${fileId}&sz=w1200`;
   }
 
   return url;
